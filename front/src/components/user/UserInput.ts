b4c3ae4a00661e03835d4e0b1c4e044e40fb6eea
kazuhito-m <kazuhito.sumpic@gmail.com>
2018-07-21 18:05:39 +0900

    実装。フィールドとデータの定義の実装。

--- front/src/components/user/UserInput.ts
+++ front/src/components/user/UserInput.ts
@@ -1,6 +1,8 @@
 export default class UserInput {
   public identifier: string = '';
   public name: string = '';
+  public dateOfBirth: string = '';
+  public phoneNumber: string = '';
 
   public readonly inputErrors: { [key: string]: string } = {};
 }
--- front/src/components/user/UserInputFields.vue
+++ front/src/components/user/UserInputFields.vue
@@ -1,30 +1,37 @@
 <template>
-<div>
+  <div>
     <InputField 
-      fieldId="name" 
+      fieldId="name"
       :value="userInput.name"
-      label="名前" 
-      placeholder="名前" 
-      :inputErrors="userInput.inputErrors" />
+      label="名前"
+      placeholder="名前"
+      :inputErrors="userInput.inputErrors"
+    />
 
-    <div th:replace="fragments/input::macro('dateOfBirth.value','生年月日','2000-01-01')">
-        <label>生年月日</label>
-        <input type="text">
-    </div>
+    <InputField
+      fieldId="dateOfBirth"
+      :value="userInput.dateOfBirth"
+      label="生年月日"
+      placeholder="2000-01-01"
+      :inputErrors="userInput.inputErrors"
+    />
 
-    <div th:replace="fragments/input::macro('phoneNumber.value','電話番号','03-1234-5678')">
-        <label>電話番号</label>
-        <input type="text">
-    </div>
+    <InputField
+      fieldId="phoneNumber"
+      :value="userInput.phoneNumber"
+      label="電話番号"
+      placeholder="03-1234-5678"
+      :inputErrors="userInput.inputErrors"
+    />
 
     <div th:replace="fragments/radio::macro('gender.value','性別','genderTypes')">
-        <div class="ui radio checkbox">
-            <label>性別</label>
-            <input type="radio" name="gender">
-            <label>不明</label>
-        </div>
+      <div class="ui radio checkbox">
+        <label>性別</label>
+        <input type="radio" name="gender">
+        <label>不明</label>
+      </div>
     </div>
-</div>
+  </div>
 </template>
 <script lang="ts">
 import { Component, Vue, Prop } from 'vue-property-decorator';
@@ -37,7 +44,10 @@ import UserInput from '@/components/user/UserInput';
   }
 })
 export default class UserInputFields extends Vue {
-  @Prop({ type: Object, default: () => new UserInput() })
+  @Prop({
+    type: Object,
+    default: () => new UserInput()
+  })
   public userInput?: UserInput;
 }
 </script>
