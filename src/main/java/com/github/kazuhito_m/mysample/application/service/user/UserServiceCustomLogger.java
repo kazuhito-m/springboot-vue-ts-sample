ba101d60b5adadafe053069ed145ca1c2c90f855
kazuhito-m <kazuhito.sumpic@gmail.com>
2019-06-11 00:09:46 +0900

    refactor: 試行錯誤でためしていた残骸を除去。

--- src/main/java/com/github/kazuhito_m/mysample/application/service/user/UserServiceCustomLogger.java
+++ src/main/java/com/github/kazuhito_m/mysample/application/service/user/UserServiceCustomLogger.java
@@ -3,9 +3,6 @@ package com.github.kazuhito_m.mysample.application.service.user;
 import com.github.kazuhito_m.mysample.application.service.aspect.JoinPointDumper;
 import com.github.kazuhito_m.mysample.application.service.logger.CustomLogger;
 import com.github.kazuhito_m.mysample.presentation.requeststock.RequestStocks;
-import com.sun.tools.attach.AttachNotSupportedException;
-import com.sun.tools.attach.VirtualMachine;
-import com.sun.tools.attach.VirtualMachineDescriptor;
 import org.aspectj.lang.JoinPoint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,10 +11,7 @@ import org.springframework.stereotype.Component;
 import javax.servlet.http.HttpServletRequest;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.management.ManagementFactory;
-import java.lang.management.RuntimeMXBean;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Component
 public class UserServiceCustomLogger implements CustomLogger {
@@ -43,45 +37,7 @@ public class UserServiceCustomLogger implements CustomLogger {
                 + " 引数:hikisuu=" + joinPoint.getArgs()[0]
                 + ",戻り値:" + returnText;
         LOGGER.info(logText);
-
-
-        LOGGER.info("threadid:" + Thread.currentThread().getId());
-
-        StackWalker stackWalker = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE);
-        stackWalker.walk(s -> s.collect(Collectors.toList()))
-                .forEach(s -> {
-                    LOGGER.info("呼び出し元:" + s);
-                    LOGGER.info("getMethodType:" + s.getMethodType().toString());
-                    LOGGER.info("idっぽいの:" + s.getByteCodeIndex());
-                    LOGGER.info("hashコード:" + s.hashCode());
-                    LOGGER.info("  toStackTraceElement().hashCode():" + s.toStackTraceElement().hashCode());
-                });
-
         writeLog();
-
-        VirtualMachine.list().forEach(i -> LOGGER.info(i.toString()));
-        VirtualMachine.list().forEach(i -> LOGGER.info(i.id()));
-
-        RuntimeMXBean bean = ManagementFactory.getRuntimeMXBean();
-        String vmName = bean.getName();
-        String id = vmName.split("@")[0];
-        LOGGER.info("カレントのPIDと思しきもの:" + id);
-
-        VirtualMachineDescriptor hitVm = VirtualMachine.list().stream()
-                .filter(vm -> vm.id().equals(id))
-                .findFirst()
-                .get();
-
-        try {
-            VirtualMachine vm = VirtualMachine.attach(id);
-            LOGGER.info("撮れたVM:" + vm);
-        } catch (AttachNotSupportedException | IOException e) {
-            LOGGER.info("VMとろうしてエラー:" + e);
-        }
-
-        LOGGER.info("VM名:" + System.getProperty("java.vm.name"));
-
-
     }
 
     private synchronized void writeLog() {
