0422a06fbb988e4611a1233fd0987fd926270e40
kazuhito-m <kazuhito.sumpic@gmail.com>
2019-06-14 23:05:45 +0900

    refactor: デバッグ用の記述削除、JavaDocコメント追加。

--- src/main/java/com/github/kazuhito_m/mysample/domain/model/config/Config.java
+++ src/main/java/com/github/kazuhito_m/mysample/domain/model/config/Config.java
@@ -1,6 +1,8 @@
 package com.github.kazuhito_m.mysample.domain.model.config;
 
-
+/**
+ * 外部設定ファイル(application.propertiesに書いてあるpathのファイル)の設定値。
+ */
 public class Config {
     Datasource mainDatasource;
 
