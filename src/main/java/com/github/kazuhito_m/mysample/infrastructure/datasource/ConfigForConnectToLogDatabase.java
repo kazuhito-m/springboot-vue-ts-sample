031860d63978018711abd5bebb399129c2406e6a
kazuhito-m <kazuhito.sumpic@gmail.com>
2019-07-12 08:46:25 +0900

    refactor: 必要のない「DIのIDを指定している」ところを削除。
    SpringDIのBeanに振られたIDの文字列的には「クラス名の先頭小文字」と同じはず…なので。

--- src/main/java/com/github/kazuhito_m/mysample/infrastructure/datasource/ConfigForConnectToLogDatabase.java
+++ src/main/java/com/github/kazuhito_m/mysample/infrastructure/datasource/ConfigForConnectToLogDatabase.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 
-@Component("configForConnectToLogDatabase")
+@Component
 public class ConfigForConnectToLogDatabase implements Config {
     final DataSource dataSource;
     final Dialect dialect;
