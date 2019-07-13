cdbce58bb970a9c71e65ec975f1ce046239d1c59
kazuhito-m <kazuhito.sumpic@gmail.com>
2019-07-13 13:30:59 +0900

    implement: Flywayで2つのデータソース、2つのマイグレーション設定で、SpringBoot起動時にマイグレーションを行う設定を付与。

---
+++ src/main/java/com/github/kazuhito_m/mysample/infrastructure/multidbsetting/FlywayConfig.java
@@ -0,0 +1,63 @@
+package com.github.kazuhito_m.mysample.infrastructure.multidbsetting;
+
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.configuration.ClassicConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.flyway.FlywayDataSource;
+import org.springframework.boot.autoconfigure.flyway.FlywayProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+import java.util.stream.Collectors;
+
+@Configuration
+public class FlywayConfig {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FlywayConfig.class);
+
+    @Bean(name = "maindbFlywayProperties")
+    @ConfigurationProperties(prefix = "maindb.flyway")
+    public FlywayProperties maindbFlywayProperties() {
+        return new FlywayProperties();
+    }
+
+    @Bean(name = "logdbFlywayProperties")
+    @ConfigurationProperties(prefix = "logdb.flyway")
+    public FlywayProperties logdbFlywayProperties() {
+        return new FlywayProperties();
+    }
+
+    @Bean(initMethod = "migrate")
+    @FlywayDataSource
+    @Primary
+    public Flyway primaryFlyway(@Qualifier("maindbFlywayProperties") FlywayProperties properties,
+                                @Qualifier("dataSource") DataSource dataSource) {
+        return createFlywayOf(properties, dataSource, "MainDB");
+    }
+
+    @Bean(initMethod = "migrate")
+    @FlywayDataSource
+    public Flyway secondaryFlyway(@Qualifier("logdbFlywayProperties") FlywayProperties properties,
+                                  @Qualifier("logDataSource") DataSource dataSource) {
+        return createFlywayOf(properties, dataSource, "LogDB");
+    }
+
+    private Flyway createFlywayOf(FlywayProperties properties, DataSource dataSource, String description) {
+        ClassicConfiguration conf = convertFlywayConfigurationOf(properties, dataSource);
+        Flyway flyway = new Flyway(conf);
+        LOGGER.info(description + " 用のDBマイグレーション(Flyway)設定を行いました。マイグレーションファイル群:" + properties.getLocations().stream().collect(Collectors.joining(",")));
+        return flyway;
+    }
+
+    private ClassicConfiguration convertFlywayConfigurationOf(FlywayProperties properties, DataSource dataSource) {
+        ClassicConfiguration conf = new ClassicConfiguration();
+        conf.setDataSource(dataSource);
+        conf.setEncoding(properties.getEncoding());
+        conf.setLocationsAsStrings(properties.getLocations().toArray(new String[]{}));
+        return conf;
+    }
+}
