0967d6fddb78c43ceb55b3d128bb20e52169c9ef
kazuhito-m <kazuhito.sumpic@gmail.com>
2019-07-13 13:47:39 +0900

    implement: Postgresで動かすために、依存性追加してプロパティ・スクリプトを修正。

--- env/local/bootRunLocalPostgres.sh
+++ env/local/bootRunLocalPostgres.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR=$(cd $(dirname $(readlink -f $0 || echo $0));pwd -P)
-cd ${SCRIPT_DIR}/../../
+cd ${SCRIPT_DIR}/../..
 
-echo ../../gradlew bootRun --args="--environment.config.path=${SCRIPT_DIR}/config-local-postgres.properties"
+./gradlew clean bootRun --args="--environment.config.path=${SCRIPT_DIR}/config-local-postgres.properties"
