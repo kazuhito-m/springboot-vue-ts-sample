ab1f9a4cc39ad4301a317a0816cf554b24a92463
kazuhito-m <kazuhito.sumpic@gmail.com>
2019-07-13 20:11:04 +0900

    refactor: 理想的には足りてない部分を追加。ソース冗長部分をまとめる。

--- src/main/java/com/github/kazuhito_m/mysample/infrastructure/multidbsetting/DataSourceConfig.java
+++ src/main/java/com/github/kazuhito_m/mysample/infrastructure/multidbsetting/DataSourceConfig.java
@@ -51,18 +51,22 @@ public class DataSourceConfig {
     public DataSourceInitializer logDataSourceInitializer(@Qualifier("logDataSource") DataSource dataSource) {
         DataSourceInitializer dataSourceInitializer = new DataSourceInitializer();
         dataSourceInitializer.setDataSource(dataSource);
-        ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator();
 
-        Resource resource = context.getResource("classpath:schema-log.sql");
-        if (resource != null && resource.exists()) {
-            databasePopulator.addScript(resource);
-        } else {
-            LOGGER.debug("DBスキーマ初期化ファイル:schema-log.sql が見つかりませんでした。初期化は行いません");
-        }
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        searchAndRegisterExecuteSql("schema-log.sql", "DBスキーマ初期化ファイル", populator);
+        searchAndRegisterExecuteSql("data-log.sql", "DBデータ初期化ファイル", populator);
+        dataSourceInitializer.setDatabasePopulator(populator);
 
-        dataSourceInitializer.setDatabasePopulator(databasePopulator);
         dataSourceInitializer.setEnabled(true);
         return dataSourceInitializer;
+    }
+
+    private void searchAndRegisterExecuteSql(String fileName, String description, ResourceDatabasePopulator populator) {
+        Resource resource = context.getResource("classpath:" + fileName);
+        if (resource != null && resource.exists()) populator.addScript(resource);
+
+        String template = "%s:%s が見つかりませんでした。初期化は行いません。";
+        LOGGER.debug(String.format(template, description, fileName));
     }
 
     public DataSourceConfig(ConfigRepository configRepository, ApplicationContext context) {
