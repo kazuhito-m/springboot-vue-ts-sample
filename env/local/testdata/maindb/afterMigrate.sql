f30e15828d88f6772eb3f7b3a28c98e98fa6ffad
kazuhito-m <kazuhito.sumpic@gmail.com>
2019-07-15 03:49:55 +0900

    refactor: スクリプトの整理とSQLの拡充。

--- env/local/testdata/maindb/afterMigrate.sql
+++ env/local/testdata/maindb/afterMigrate.sql
@@ -5,5 +5,6 @@ INSERT INTO users.users
 (user_id, name, phone_number, date_of_birth, gender)
 VALUES
  ('hoge@example.com', 'テストユーザ01', '03-1234-5678','1977-08-17','男性'),
- ('fuga@example.com', 'テストユーザ02', '03-1234-5678','1977-08-17','女性');
- ('buhi@example.com', 'テストユーザ03', '03-1234-5678','1977-08-17','不明');
+ ('fuga@example.com', 'テストユーザ02', '03-1234-5678','1977-08-17','女性'),
+ ('buhi@example.com', 'テストユーザ03', '03-1234-5678','1977-08-17','不明'),
+ ('ahei@example.com', 'テストユーザ04', '03-1234-5678','1977-08-17','男性');
