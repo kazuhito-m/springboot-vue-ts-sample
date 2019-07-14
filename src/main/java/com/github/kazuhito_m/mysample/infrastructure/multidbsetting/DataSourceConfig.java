b278fd06cc4dfe1ffc17fb24fb5c44c297c659ae
kazuhito-m <kazuhito.sumpic@gmail.com>
2019-07-15 02:25:40 +0900

    implement: 「外部から指定できるスタートアップSQL」を設定・実行できる仕組みを追加。

--- src/main/java/com/github/kazuhito_m/mysample/infrastructure/multidbsetting/DataSourceConfig.java
+++ src/main/java/com/github/kazuhito_m/mysample/infrastructure/multidbsetting/DataSourceConfig.java
@@ -6,16 +6,23 @@ import com.github.kazuhito_m.mysample.domain.model.config.Datasource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.jdbc.datasource.init.DataSourceInitializer;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
 import javax.sql.DataSource;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * 複数データベースかつ接続情報を設定ファイル以外から取得するコンフィグ。
@@ -26,6 +33,8 @@ public class DataSourceConfig {
 
     private final ConfigRepository configRepository;
     private final ApplicationContext context;
+
+    private final String startupSqlPaths;
 
     @Bean("dataSource")
     @Primary
@@ -49,30 +58,83 @@ public class DataSourceConfig {
         return builder.build();
     }
 
-    @Bean("logDataSourceInitializer")
-    public DataSourceInitializer logDataSourceInitializer(@Qualifier("logDataSource") DataSource dataSource) {
+    @Bean("dataSourceInitializer")
+    @Primary
+    public DataSourceInitializer dataSourceInitializer(@Qualifier("dataSource") DataSource dataSource) {
+        List<String> sqlFilePaths = sqlPaths();
+        if (sqlFilePaths.isEmpty()) return null;
+
         DataSourceInitializer dataSourceInitializer = new DataSourceInitializer();
         dataSourceInitializer.setDataSource(dataSource);
 
         ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-        searchAndRegisterExecuteSql("schema-log.sql", "DBスキーマ初期化ファイル", populator);
-        searchAndRegisterExecuteSql("data-log.sql", "DBデータ初期化ファイル", populator);
+        for (String path : sqlFilePaths) searchAndRegisterExecuteSql(path, "MainDBデータ初期化ファイル(オプション)", populator);
         dataSourceInitializer.setDatabasePopulator(populator);
 
         dataSourceInitializer.setEnabled(true);
         return dataSourceInitializer;
     }
 
-    private void searchAndRegisterExecuteSql(String fileName, String description, ResourceDatabasePopulator populator) {
+    private void searchAndRegisterExecuteSql(String path, String description, ResourceDatabasePopulator populator) {
+        Resource resource = searchFile(path);
+        if (resource == null || !resource.exists()) {
+            String template = "%s:%s が設定されていますが見つかりませんでした。SQL実行は行いません。";
+            LOGGER.info(String.format(template, description, path));
+            return;
+        }
+
+        String template = "%s:%s が見つかりました。SQLを実行します。";
+        LOGGER.info(String.format(template, description, resource));
+
+        populator.addScript(resource);
+    }
+
+    private Resource searchFile(String path) {
+        Resource resource = context.getResource(path);
+        if (resource != null && resource.exists()) return resource;
+
+        resource = context.getResource("classpath:" + path);
+        if (resource != null && resource.exists()) return resource;
+
+        resource = new FileSystemResource(path);
+        return resource;
+    }
+
+
+    private List<String> sqlPaths() {
+        if (startupSqlPaths == null) return Collections.emptyList();
+        return Stream.of(startupSqlPaths.split(","))
+                .map(String::trim)
+                .collect(toList());
+    }
+
+    @Bean("logDataSourceInitializer")
+    public DataSourceInitializer logDataSourceInitializer(@Qualifier("logDataSource") DataSource dataSource) {
+        DataSourceInitializer dataSourceInitializer = new DataSourceInitializer();
+        dataSourceInitializer.setDataSource(dataSource);
+
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        searchAndRegisterExecuteSqlOnClassPath("schema-log.sql", "DBスキーマ初期化ファイル", populator);
+        searchAndRegisterExecuteSqlOnClassPath("data-log.sql", "DBデータ初期化ファイル", populator);
+        dataSourceInitializer.setDatabasePopulator(populator);
+
+        dataSourceInitializer.setEnabled(true);
+        return dataSourceInitializer;
+    }
+
+    private void searchAndRegisterExecuteSqlOnClassPath(String fileName, String description, ResourceDatabasePopulator populator) {
         Resource resource = context.getResource("classpath:" + fileName);
         if (resource != null && resource.exists()) populator.addScript(resource);
 
         String template = "%s:%s が見つかりませんでした。初期化は行いません。";
-        LOGGER.debug(String.format(template, description, fileName));
+        LOGGER.info(String.format(template, description, fileName));
     }
 
-    public DataSourceConfig(ConfigRepository configRepository, ApplicationContext context) {
+    public DataSourceConfig(ConfigRepository configRepository,
+                            ApplicationContext context,
+                            @Value("${maindb.startup.sql.paths}") String startupSqlPaths) {
         this.configRepository = configRepository;
         this.context = context;
+        this.startupSqlPaths = startupSqlPaths;
     }
 }
