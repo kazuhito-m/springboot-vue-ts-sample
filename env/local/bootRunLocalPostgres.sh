954719139126038e9876e1989a3bbdfdbfcbd4d6
kazuhito-m <kazuhito.sumpic@gmail.com>
2019-07-15 03:25:47 +0900

    implment: 起動前にFlywayの仕組みを使ってSQLを実行する仕組みを、起動スクリプトに追加。
    結局のところ、これが一番簡単そうだったので、下手に実装せずに設定値でなんとかする。

--- env/local/bootRunLocalPostgres.sh
+++ env/local/bootRunLocalPostgres.sh
@@ -3,4 +3,7 @@
 SCRIPT_DIR=$(cd $(dirname $(readlink -f $0 || echo $0));pwd -P)
 cd ${SCRIPT_DIR}/../..
 
-./gradlew clean bootRun --args="--environment.config.path=${SCRIPT_DIR}/config-local-postgres.properties"
+args="--environment.config.path=${SCRIPT_DIR}/config-local-postgres.properties"
+args="${args} --maindb.flyway.locations=classpath:db/migration/maindb,filesystem:${SCRIPT_DIR}/testdata/maindb"
+
+./gradlew clean bootRun --args=${args}
---
+++ env/local/testdata/maindb/afterMigrate.sql
@@ -0,0 +1,9 @@
+DELETE FROM profiles.profile_images;
+DELETE FROM users.users;
+
+INSERT INTO users.users
+(user_id, name, phone_number, date_of_birth, gender)
+VALUES
+ ('hoge@example.com', 'テストユーザ01', '03-1234-5678','1977-08-17','男性'),
+ ('fuga@example.com', 'テストユーザ02', '03-1234-5678','1977-08-17','女性');
+ ('buhi@example.com', 'テストユーザ03', '03-1234-5678','1977-08-17','不明');
